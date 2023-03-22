ff1614612e13dc3ac000e472e5c3477a3c2b559c
ROMAN <junk0410448@mail.ru>
2023-03-22 19:13:28 +0700

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,24 @@
+﻿ Console.WriteLine("Введите пятизначное число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int originalNumber = number;
+ int reverseNumber = 0;
+  if (number < 10000 || number >= 100000)
+  {
+      Console.WriteLine("Вы ввели не пятизначное число, попробуйте еще раз");
+      return;
+  }
+ while (number > 0)
+ {
+     int lastDigit = number % 10;
+     reverseNumber = reverseNumber * 10 + lastDigit;
+     number /=10;
+ }
+ if ( originalNumber == reverseNumber)
+ {
+     Console.WriteLine (originalNumber + " - является полиндромом  " );
+ }
+ else
+ {
+     Console.WriteLine (originalNumber +  " - не является полиндромом  " );
+ }
+
